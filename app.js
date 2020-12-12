278f2ac4dd31cc159169b5e2226340f6c27c0fa4
Muhamamd Salif <muhammad.salif@yahoo.com>
2020-12-13 01:47:37 +0500

    Zip code route implemented.

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const port = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 
+var weather = require('openweather-apis');
+var ownKey = "2617c465639f83ad21809927b4a2d0d3"
 
+weather.setAPPID(ownKey);
 
-
-
-
-
-
+app.get(/^\/(\zipcode)\/(\d{5})$/, (req, res) => {
+    weather.setZipCode(req.params[1]);
+    weather.getAllWeather(function (err, JSONObj) {
+        console.log(JSONObj);
+        res.send(JSONObj)
+    });
+})
 
 app.listen(port, () => {
     console.log('Express app is listening at the http://localhost:', port)
